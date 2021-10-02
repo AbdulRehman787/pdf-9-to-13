59faa142255b0707ccb3fb21d29d17625edfd3ba
AbdulRehman787 <88674059+AbdulRehman787@users.noreply.github.com>
2021-10-02 11:15:17 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,142 @@
+// var a = prompt("Enter your name")
+// console.log(a)
+
+// var city = prompt("Enter your city name");
+// if (city == "Karachi") {
+//     alert("Welcome to the city of light")
+// } else {
+//     alert("Your city is clean")
+// }
+
+// var gender = prompt("Enter your gender")
+// if (gender == "Male") {
+//     alert("Good Morning Sir")
+// } else if (gender == "Female") {
+//     alert("Good morning Mam")
+// } else {
+//     alert("Type your gender")
+// }
+
+// var signalColor = prompt("Enter anyone signal color", "Ex Red ,yellow, Green")
+// if (signalColor == "Red") {
+//     alert("Must Stop")
+// } else if (signalColor == "Yellow") {
+//     alert("Ready To Move")
+// } else if (signalColor == "Green") {
+//     alert("Move Away")
+// } else {
+//     alert("Enter any one signal Color")
+// }
+
+// var a = +prompt("Enter your  fuel in remaining in your car")
+// if (a == "1") {
+//     alert("your car is going to 5 kilometer")
+// } else if (a == "2") {
+//     alert("your car is going to 10 kilometer")
+// } else if (a == "3") {
+//     alert("Your car is going to 13 kilometer")
+// } else if (a == "4") {
+//     alert("Your car is going to 17 kilometer")
+// } else if (a == "5") {
+//     alert("Your car is going to 20 kilometer")
+// } else {
+//     alert("Go to fuel pump and give his fuel")
+// }
+
+// var a = 4;
+// if (++a === 5) {
+//     alert("a condition value is true")
+// }
+// var b = 82;
+// if (++b === 83) {
+//     alert("condition value b is true")
+// }
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost
+// if (totalCost === materialCost + laborCost) {
+//     alert("condition is true")
+// }
+// console.log(true)
+// if (true) {
+//     alert("true")
+// }
+// if (false) {
+//     alert("flase")
+// }
+
+// if ("car" < "cat") {
+//     alert("car is smaller than  cat")
+// }
+
+
+
+// var subjec1 = prompt("Enter your English number");
+// var subjec2 = prompt("Enter your Urdu number");
+// var subjec3 = prompt("Enter your Math number");
+// var subjec4 = prompt("Enter your Physic number");
+// var subjec5 = prompt("Enter your Chemistry number");
+// var totalNumber = 500;
+// var obtMark = (subjec1 + subjec2 + subjec3 + subjec4 + subjec5);
+// var percentage = obtMark / totalNumber;
+// document.write(percentage)
+
+
+// var user = +prompt("Enter the number 1 to 10 ");
+// var num = 8;
+// if (user == num) {
+//     alert("Bingo Correct answer");
+// } else {
+//     alert("Please try again");
+// }
+
+// var userinput = prompt("Enter the numberof 1 to 10");
+// var num = 9;
+// if (userinput == num) {
+//     alert("You are winner")
+// } else {
+//     alert("Try again")
+// }
+
+// var user = prompt("Enterthe anyone number you check this number is dividible by 3 yes ya no");
+// if (user % 3 == 0) {
+//     alert("This number is dividble by 3")
+// } else {
+//     alert("This number is notdivisble by 3 ")
+// }
+
+// var userinput = prompt("Enter the number check this even number ya odd number")
+// if (userinput % 2 == 0) {
+//     alert("This is even Number")
+// } else {
+//     alert("This is odd number")
+// }
+
+// var temperature = prompt("Enter the your area temperature")
+// if (temperature < 40 == 0) {
+//     alert("“It is too hot outside.”")
+// } else if (temperature < 30 == 0) {
+//     alert("“The Weather today is Normal.”")
+// } else if (temperature < 20 == 0) {
+//     alert("Today weather is cool")
+// } else if (temperature < 10) {
+//     alert("Today weather is So Coll")
+// } else {
+//     alert("OMG!")
+// }
+
+
+
+var temperature = prompt("Enter the your area temperatre");
+if (temperature < 40 == 0) {
+    alert("“It is too hot outside.”")
+} else if (temperature < 30 == 0) {
+    alert("“The Weather today is Normal.”")
+} else if (temperature < 20 == 0) {
+    alert("“Today’s Weather is cool.”")
+} else if (temperature < 10) {
+    alert("“OMG! Today’s weather is so Cool.”")
+} else {
+    alert("OMG!")
+}
